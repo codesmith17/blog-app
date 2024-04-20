6a3bed44239fe8d1386de1fbbda81da910847d64
codesmith17 <kanchanlatakrishna@gmail.com>
2024-04-20 12:27:50 +0530

    mongo

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv")
+const mongoose = require("mongoose");
+dotenv.config();
+mongoose.connect(process.env.MONGO)
+    .then(res => {
+        console.log("CONNECTED");
+    })
+    .catch(err => console.log(err))
 app.listen(3000, () => {
     console.log("SERVER IS RUNNING ON PORT 3000");
 })
